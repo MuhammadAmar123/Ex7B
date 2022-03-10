d457c3b6deba84f945c21e0c7a8c2c94b820460b
ac116681 <ac116681@8CG8163KX3>
2022-03-10 15:13:33 +1300

    Add project files.

---
+++ Ex7B/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Enter number from 0-100 to show grade: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 39)
+{
+    Console.WriteLine("Grade is: E");
+}
+else if (num <= 49)
+{
+    Console.WriteLine("Grade is: D");
+}
+else if (num <= 56)
+{
+    Console.WriteLine("Grade is: C");
+}
+else if (num <= 63)
+{
+    Console.WriteLine("Grade is: C+");
+}
+else if (num <= 70)
+{
+    Console.WriteLine("Grade is: B");
+}
+else if (num <= 77)
+{
+    Console.WriteLine("Grade is: B+");
+}
+else if (num <= 84)
+{
+    Console.WriteLine("Grade is: A");
+}
+else if (num <= 100)
+{
+    Console.WriteLine("Grade is: A+");
+}
